9ba61f12dcf0b67296bbc6d1ceb0c2f71316db89
Raphael Goldenberg <168562868+raphaelggg@users.noreply.github.com>
2026-02-12 18:29:26 +0100

    Add files via upload

---
+++ ft_atoi.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ragolden <ragolden@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/10 16:50:41 by ragolden          #+#    #+#             */
+/*   Updated: 2025/11/13 14:48:03 by ragolden         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	res;
+
+	i = 0;
+	sign = 1;
+	res = 0;
+	while ((str[i] == ' ') || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + str[i] - '0';
+		i++;
+	}
+	return (sign * (int)res);
+}
+
+/*#include <stdio.h>
+
+int main()
+{
+	printf("%d", ft_atoi("+-54"));
+}*/
